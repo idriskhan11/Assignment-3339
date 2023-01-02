a7e41a8dbd73b8a22fbb943674672a9722d114e9
idriskhan11 <110292084+idriskhan11@users.noreply.github.com>
2023-01-02 17:13:13 +0530

    Add files via upload

---
+++ C# assignments/ArrayAssignment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ArrayAssignment
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Write a function that takes an array of numbers and it should display the Odd and Even numbers
+            int[] values = { 1, 5, 3, 9, 8, 24, 25, 33, 21, 44 };
+
+            foreach (var result in values)
+            {
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine(result + " is a Even Value");
+                }
+                else
+                {
+                    Console.WriteLine(result + " is a Odd Value");
+                }
+            }
+        }
+    }
+}
+
+
---
+++ C# assignments/calculater.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+    class Calculater
+    {
+        static void Main(string[] args)
+        {
+
+            bool value = true;
+            while (value)
+            {
+                Console.WriteLine("enter the first value for calculation");
+
+                int firstValue = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter the second value for calculation");
+                int secondValue = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("Enter the operator +, -, * ,/");
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        Console.WriteLine("the sum of given values is " + (firstValue + secondValue));
+                        break;
+
+                    case "-":
+                        Console.WriteLine("the difference of given values is " + (firstValue - secondValue));
+                        break;
+
+                    case "*":
+                        Console.WriteLine("the product of given values is " + (firstValue * secondValue));
+                        break;
+
+                    case "/":
+                        if (secondValue != 0)
+                        {
+                            Console.WriteLine("the division of given values is " + (firstValue / secondValue));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Arithematic Exception");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("enter correct option");
+                        break;
+
+
+                }
+                Console.WriteLine("<<<<>>>>");
+                Console.WriteLine("enter 0 to Exit");
+                Console.WriteLine("type any number to continue");
+                int opt = Convert.ToInt32(Console.ReadLine());
+                if (opt == 0)
+                   value = false;
+            }
+            Console.WriteLine("<<<<>>>>>");
+            Console.WriteLine("calculation terminated");
+        }
+    }
+}
+
