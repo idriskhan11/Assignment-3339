bbd1f2dc7b23f659744f36f2b5f7640f3cea3dbc
idriskhan11 <110292084+idriskhan11@users.noreply.github.com>
2023-01-05 17:37:47 +0530

    Add files via upload

---
+++ C# assignments/stingReverse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class stingReverse
+    {
+       
+        
+            static string reverseString(string input)
+            {
+                string[] arr = input.Split(' ');
+                string output = "";
+
+                for (int i = arr.Length - 1; i >= 0; i--)
+                {
+                    output += arr[i];
+                    output += " ";
+
+                }
+
+                return output;
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("enter the input to reverse");
+                string input = Console.ReadLine();
+                string output = reverseString(input);
+
+                Console.WriteLine(output);
+            }
+        }
+    }
+ 
+   
+
